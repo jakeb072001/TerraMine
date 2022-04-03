c06d9abf5acea40d61159d71ae3cebe1836a153c
jakeb072001 <crazyminecraft48@gmail.com>
2022-04-04 04:29:46 +1000

    Added new Advancements and Changed old one

--- src/main/java/terracraft/common/item/magic/ManaCrystalItem.java
+++ src/main/java/terracraft/common/item/magic/ManaCrystalItem.java
@@ -1,6 +1,9 @@
 package terracraft.common.item.magic;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
@@ -25,9 +28,14 @@ public class ManaCrystalItem extends TerrariaItemConfigurable {
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entity) {
         Player player = (Player)entity;
+        if (player instanceof ServerPlayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer)player, stack);
+        }
+
         if (ModComponents.MANA_HANDLER.get(player).getMaxMana() < 200) {
             ModComponents.MANA_HANDLER.get(player).addMaxMana(20);
             ModComponents.MANA_HANDLER.get(player).addCurrentMana(20);
+            player.awardStat(Stats.ITEM_USED.get(this));
             if (!player.isCreative()) {
                 stack.shrink(1);
             }
